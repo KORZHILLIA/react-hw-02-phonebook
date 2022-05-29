9cf6f6b2a27a393f7c1e03ca6fe29f936799501e
KORZH ILLIA <kiev_drum2006@ukr.net>
2022-05-29 22:20:32 +0300

    Done.

--- src/components/Phonebook/ContactsForm/ContactsForm.js
+++ src/components/Phonebook/ContactsForm/ContactsForm.js
@@ -29,39 +29,42 @@ class ContactsForm extends Component {
   render() {
     const { name, number } = this.state;
     return (
-      <form>
-        <label>
-          Name
-          <input
-            value={name}
-            onChange={this.onNameChangeHandler}
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-          />
-        </label>
-        <label>
-          Number
-          <input
-            onChange={this.onNumberChangeHandler}
-            value={number}
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-          />
-        </label>
-        <button
-          type="submit"
-          disabled={this.isBtnDisabled()}
-          onClick={this.submitHandler}
-        >
-          Add contact
-        </button>
-      </form>
+      <>
+        <h1>Phonebook</h1>
+        <form>
+          <label>
+            Name
+            <input
+              value={name}
+              onChange={this.onNameChangeHandler}
+              type="text"
+              name="name"
+              pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+              title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+              required
+            />
+          </label>
+          <label>
+            Number
+            <input
+              onChange={this.onNumberChangeHandler}
+              value={number}
+              type="tel"
+              name="number"
+              pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+              title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+              required
+            />
+          </label>
+          <button
+            type="submit"
+            disabled={this.isBtnDisabled()}
+            onClick={this.submitHandler}
+          >
+            Add contact
+          </button>
+        </form>
+      </>
     );
   }
 }
--- src/components/Phonebook/Phonebook.js
+++ src/components/Phonebook/Phonebook.js
@@ -1,41 +1,35 @@
 import { Component } from 'react';
 import ContactsForm from './ContactsForm/ContactsForm';
-import Contact from './Contact/Contact';
+import FilterInput from './FilterInput/FilterInput';
+import Contacts from './Contacts/Contacts';
 
 class Phonebook extends Component {
   state = {
-    name: '',
     contacts: [
       { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
       { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
       { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
       { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
-    number: '',
     filter: '',
   };
 
-  // onSubmit = event => {
-  //   event.preventDefault();
-  //   return this.setState(prevState => {
-  //     return {
-  //       contacts: [
-  //         ...prevState.contacts,
-  //         { name: prevState.name, id: nanoid(), number: prevState.number },
-  //       ],
-  //     };
-  //   });
-  // };
-
   onSubmit = obj =>
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, obj],
-    }));
-
-  contactsFindHandler = event =>
-    this.setState({
-      filter: event.currentTarget.value,
+    this.setState(prevState => {
+      const contactIndex = prevState.contacts.findIndex(
+        ({ name }) => name.toLowerCase() === obj.name.toLowerCase()
+      );
+      if (contactIndex === -1) {
+        return {
+          contacts: [...prevState.contacts, obj],
+        };
+      } else {
+        alert('no');
+      }
     });
+
+  filterChangeHandler = event =>
+    this.setState({ filter: event.currentTarget.value });
 
   getFilteredContacts = () => {
     const requiredName = this.state.filter.toLowerCase();
@@ -46,26 +40,28 @@ class Phonebook extends Component {
     ];
   };
 
+  deleteBtnHandler = contactId =>
+    this.setState(prevState => {
+      const contactIndex = prevState.contacts.findIndex(
+        ({ id }) => id === contactId
+      );
+      prevState.contacts.splice(contactIndex, 1);
+      return { contacts: [...prevState.contacts] };
+    });
+
   render() {
     const { filter } = this.state;
     return (
       <div>
-        <h1>Phonebook</h1>
         <ContactsForm onSubmit={res => this.onSubmit(res)}></ContactsForm>
-        <label>
-          Find contacts by name
-          <input
-            type="text"
-            onChange={this.contactsFindHandler}
-            value={filter}
-          ></input>
-        </label>
-        <p>Contacts</p>
-        <ul>
-          {this.getFilteredContacts().map(({ name, id, number }) => (
-            <Contact name={name} id={id} number={number}></Contact>
-          ))}
-        </ul>
+        <FilterInput
+          filter={filter}
+          changeHandler={this.filterChangeHandler}
+        ></FilterInput>
+        <Contacts
+          contactsArray={this.getFilteredContacts()}
+          onDelete={res => this.deleteBtnHandler(res)}
+        ></Contacts>
       </div>
     );
   }
